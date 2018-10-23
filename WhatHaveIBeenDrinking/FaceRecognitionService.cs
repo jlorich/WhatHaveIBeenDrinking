af8a0b532893503031800d32829c0e89cfd8bfd2
Joey Lorich <joseph.lorich@microsoft.com>
2018-10-23 10:28:44 -0600

    initial commit

---
+++ WhatHaveIBeenDrinking/FaceRecognitionService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.CognitiveServices.Vision.Face;
+using Windows.Storage;
+
+namespace WhatHaveIBeenDrinking
+{
+    public class FaceRecognitionService
+    {
+        private const string faceAccessKey = "";
+        private const string faceEndpoint = "https://westcentralus.api.cognitive.microsoft.com";
+
+        public async void Recognize(StorageFile photo)
+        {
+
+        }
+    }
+}
--- WhatHaveIBeenDrinking/MainPage.xaml.cs
+++ WhatHaveIBeenDrinking/MainPage.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using WhatHaveIBeenDrinking.Controls;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -24,7 +26,71 @@ namespace WhatHaveIBeenDrinking
     {
         public MainPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+            //var cameraCaptureService = new CameraCaptureService();
+            //var faceRecognitionService = new FaceRecognitionService();
+
+            //var photo = cameraCaptureService.Capture().GetAwaiter().GetResult();
+            //faceRecognitionService.Recognize(photo);
+        }
+
+        private async void CurrentWindowActivationStateChanged(object sender, Windows.UI.Core.WindowActivatedEventArgs e)
+        {
+            if ((e.WindowActivationState == Windows.UI.Core.CoreWindowActivationState.CodeActivated ||
+                e.WindowActivationState == Windows.UI.Core.CoreWindowActivationState.PointerActivated) &&
+                cameraControl.CameraStreamState == Windows.Media.Devices.CameraStreamState.Shutdown)
+            {
+                // When our Window loses focus due to user interaction Windows shuts it down, so we 
+                // detect here when the window regains focus and trigger a restart of the camera.
+                await cameraControl.StartStreamAsync(isForRealTimeProcessing: true);
+            }
+        }
+
+        private void OnFaceDetected(object sender, FaceDetectedEventArgs args)
+        {
+            var face = args.Face;
+            Console.Out.WriteLine("Face detected");
+
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            cameraControl.FaceDetected += OnFaceDetected;
+
+            await cameraControl.StartStreamAsync(isForRealTimeProcessing: true);
+
+            //EnterKioskMode();
+
+            //if (string.IsNullOrEmpty(SettingsHelper.Instance.FaceApiKey))
+            //{
+            //    await new MessageDialog("Missing Face API Key. Please enter a key in the Settings page.", "Missing API Key").ShowAsync();
+            //}
+            //else
+            //{
+            //    FaceListManager.FaceListsUserDataFilter = SettingsHelper.Instance.WorkspaceKey + "_RealTime";
+            //    await FaceListManager.Initialize();
+
+            //    await ResetDemographicsData();
+            //    this.UpdateDemographicsUI();
+
+                
+            //    this.StartProcessingLoop();
+            //}
+
+            ////get a reference to SenseHat
+            //try
+            //{
+            //    senseHat = await SenseHatFactory.GetSenseHat();
+
+            //    senseHat.Display.Clear();
+            //    senseHat.Display.Update();
+
+            //}
+            //catch (Exception)
+            //{
+            //}
+
+            base.OnNavigatedTo(e);
         }
     }
 }
